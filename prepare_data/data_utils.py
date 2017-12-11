4f31f01e42bc3538d63f301a5472f8aed59d9892
Ludovic Trottier <ludovic.trottier.2@gmail.com>
2017-12-11 11:19:34 -0500

    Removed IoU

--- prepare_data/data_utils.py
+++ prepare_data/data_utils.py
@@ -106,32 +106,6 @@ def read_and_write_annotation(base_dir, dir):
 def get_path(base_dir, filename):
     return os.path.join(base_dir, filename)
 
-def IoU(box, bboxes):
-    """
-    Caculate IoU between detect and ground truth boxes
-    :param crop_box:numpy array (4, )
-    :param bboxes:numpy array (n, 4):x1, y1, x2, y2
-    :return:
-    numpy array, shape (n, ) Iou
-    """
-    box_area = (box[2] - box[0] + 1) * (box[3] - box[1] + 1)
-    areas = (bboxes[:, 2] - bboxes[:, 0] + 1) * (bboxes[:, 3] - bboxes[:, 1] + 1)
-    xx1 = np.maximum(box[0], bboxes[:, 0])
-    yy1 = np.maximum(box[1], bboxes[:, 1])
-    xx2 = np.minimum(box[2], bboxes[:, 2])
-    yy2 = np.minimum(box[3], bboxes[:, 3])
-
-    # compute the width and height of the bounding box
-    w = np.maximum(0, xx2 - xx1 + 1)
-    h = np.maximum(0, yy2 - yy1 + 1)
-
-    inter = w * h
-    over = inter / (box_area + areas - inter)
-
-    return over
-
-
-
 
 if __name__ == '__main__':
     dir = '/media/thinkjoy/新加卷/dataset/widerface/wider_face_split/wider_face_train_bbx_gt.txt'
